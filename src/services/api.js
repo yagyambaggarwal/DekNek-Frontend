022b02db31f1ec3271476cf3d89394a26e5de7c2
yagyam <vishuaggarwal219@gmail.com>
2026-04-24 02:35:37 +0530

    update API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://deknek-backend-65ck.onrender.com/api/v1",
     withCredentials: true
 });
